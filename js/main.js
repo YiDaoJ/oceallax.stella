4edd3c2fe6f26ea5086ba07c07f609658cf37043
YiDaoJ <jcxsusan@gmail.com>
2017-07-04 22:03:14 +0200

    change loading animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
-// $(window).load(ladenAnimation);
-// 
-
 $(document).ready(function(){
-	var s = skrollr.init();
-	// console.log('test');
-	// ladenAnimation();
-	// console.log(window.pageYOffset);
+	var s = skrollr.init();	
+  var scroll = window.pageYOffset;
+
+  // playSound();
 });
+
+// function playSound() {
+//   if(scroll > 31000 && scroll < 34500) {
+//     console.log("test");
+//     $('#bubble_sound').play();
+//   }
+// }
+
+function loadingCheck() {
+  if (document.readyState != "complete") {
+    showIntro();
+    window.setTimeout(loadingCheck, 10000);    
+  }
+  else {
+    document.getElementById('loading').id = 'loaded';
+  }
+}
+
+function showIntro() {
+  $('#intro').fadeIn('slow', function() {
+    $(this).css('display', 'block'); 
+    $('#loading').css('display','none');
+  });				
+}
 
